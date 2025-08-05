edd58116df59e7e8fac7b5da99eaddaac1e57485
egbog <rysimpry@yahoo.com>
2025-08-05 16:58:39 -0700

    Update DisableScavModePatch.cs

--- Patches/DisableScavModePatch.cs
+++ Patches/DisableScavModePatch.cs
@@ -8,13 +8,12 @@ namespace _DisableScavMode_egboggied.Patches;
 
 public class DisableScavModePatch : ModulePatch {
     protected override MethodBase GetTargetMethod() {
-        return typeof(MatchMakerSideSelectionScreen).GetMethod("Awake",
-            BindingFlags.Public | BindingFlags.Instance);
+        return typeof(MatchMakerSideSelectionScreen).GetMethod("Awake", BindingFlags.Public | BindingFlags.Instance);
     }
 
     [PatchPostfix]
     public static void Postfix(MatchMakerSideSelectionScreen __instance, Button ____savagesBigButton,
-                               Button                                          ____pmcBigButton) {
+                               Button                        ____pmcBigButton) {
         ____savagesBigButton.transform.parent.gameObject.SetActive(false);
         ____pmcBigButton.transform.parent.transform.localPosition = new Vector3(-220, 520, 0);
     }
